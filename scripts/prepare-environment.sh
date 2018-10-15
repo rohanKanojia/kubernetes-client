17fc024350386eb425869413233d252644e7144c
Rohan Kumar <rohaan@redhat.com>
2018-10-15 16:45:25 +0530

    Test prepare script

---
+++ scripts/prepare-environment.sh
@@ -0,0 +1,9 @@
+
+echo $SETTINGS_XML | base64 --decode > $HOME/.m2/settings.xml
+cat $HOME/.m2/settings.xml
+echo $PUBRING | base64 --decode > $HOME/.gnupg/pubring.gpg
+cat $HOME/.gnupg/pubring.gpg
+echo $SEC_JENKINS |  base64 --decode > $HOME/.gnupg/sec_jenkins.gpg
+cat $HOME/.gnupg/sec_jenkins.gpg
+echo $SECRING | base64 --decode > $HOME/.gnupg/secring.gpg
+echo $TRUSTDB | base64 --decode > $HOME/.gnupg/trustdb.gpg
